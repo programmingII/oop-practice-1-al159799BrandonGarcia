6f4db7e5c6a88637391ac4729b35a520ad5042f5
al159799-BrandonGarcia <46730068+al159799BrandonGarcia@users.noreply.github.com>
2019-02-18 00:05:11 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio31.java
@@ -0,0 +1,16 @@
+//Nombre:Brandon Garcia
+//Fecha:02/17/2019
+//Hora:11:58
+
+public class Ejercicio31{
+public static void main(String[] args){//declaracion de main
+
+    System.out.println("\nJava Version: "+System.getProperty("java.version"));//print que imprime la version de java
+    System.out.println("Java Runtime Version: "+System.getProperty("java.runtime.version"));//print que imprime la version de runtime
+    System.out.println("Java Home: "+System.getProperty("java.home"));//print que imprime la propiedad de java
+    System.out.println("Java Vendor: "+System.getProperty("java.vendor"));//print que imprime el proovedor
+    System.out.println("Java Vendor URL: "+System.getProperty("java.vendor.url"));//print que imprime la url del proovedor
+    System.out.println("Java Class Path: "+System.getProperty("java.class.path")+"\n");//print que imprime el path de java 
+
+}
+}
