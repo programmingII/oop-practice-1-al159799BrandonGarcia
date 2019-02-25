f61b10012ed5d956b19c267b0604d1a8593a761a
al159799-BrandonGarcia <46730068+al159799BrandonGarcia@users.noreply.github.com>
2019-02-24 21:22:51 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio44.java
@@ -0,0 +1,15 @@
+//Nombre:Brandon Garcia
+//Fecha:24/02/2019
+//Hora:9:00pm
+
+import java.util.Scanner;//importacion de la libreria scanner
+
+public class Ejercicio44{
+public static void main(String[] args) {//inicializacion del main
+Scanner in = new Scanner(System.in);//declaracion de scanner
+
+System.out.println("Ingrese un numero");//print que indica que se ingrese un numero
+int A = in.nextInt();//declaracion de una variable entera que recibira el valor dado por el usurio
+System.out.printf("%d + %d%d + %d%d%d\n", A,A,A,A,A,A);//print que imprime el numero ingresado
+ }
+}
