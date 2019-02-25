bb4aba5a79ef6a7fbca785b878a3cae10bb0cb9d
al159799-BrandonGarcia <46730068+al159799BrandonGarcia@users.noreply.github.com>
2019-02-24 21:05:09 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio43.java
@@ -0,0 +1,11 @@
+//Nombre:Brandon Garcia
+//Fecha:24/02/2019
+//Hora:9:00pm
+
+
+public class Ejercicio43{
+public static void main(String[] args) {//inicializacion del main
+
+System.out.println("Agua, ¿donde vas?\n \tRiendo voy por el rion\n \t\ta las orillas del mar\n \t\t\tMar, ¿adonde vas?\n Rio arriba voy buscando\n \tfuente donde descansar\n \t\tChopo, y tu ¿que haras?\n \t\t\tNo quiero decirte nada\n Yo…, ¡temblar!\n \t¿Que deseo, que no deseo\n \t\tpor el río y por la mar?\n \t\t\tCuatro pajaros sin rumbo\n en el alto chopo estan.");
+ }
+}
