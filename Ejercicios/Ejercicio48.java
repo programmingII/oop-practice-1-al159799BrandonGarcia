9be1caf4cbe66bb4a86ec8f51274643b6fbeb48c
al159799-BrandonGarcia <46730068+al159799BrandonGarcia@users.noreply.github.com>
2019-02-25 20:05:03 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio48.java
@@ -0,0 +1,14 @@
+//Nombre:Brandon Garcia
+//Fecha:25/02/2019
+//Hora:7:44pm
+
+public class Ejercicio48{
+public static void main(String[] args){//inicializacion de main
+
+for(int i = 1; i <= 99; i++){//ciclo for para aumentar el valor de la variable i de 1 a 99
+if(i % 2 != 0)//condicional if que su condicion se cumple si el resuido de una division de la variable i entre 2 no da 0 
+System.out.println(i);//print que muestra los numeros impares 
+}
+
+ }
+}
