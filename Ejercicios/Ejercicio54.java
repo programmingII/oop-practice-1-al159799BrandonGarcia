af1dbca881ce80688323b088b4ce7d5a2b143938
al159799-BrandonGarcia <46730068+al159799BrandonGarcia@users.noreply.github.com>
2019-02-26 13:27:25 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio54.java
@@ -0,0 +1,24 @@
+//Nombre:Brandon Garcia
+//Fecha:26/02/2019
+//Hora:1:15pm
+
+import java.util.Scanner;//importacion de la libreria scanner 
+
+public class Ejercicio54{
+public static void main(String[] args){//inicializacion de main
+Scanner in = new Scanner(System.in);//declaracion de un Scanner
+
+System.out.println("Ingrese el primer numero");//print que le pide al usuario que ingrese un numero
+int A = in.nextInt();//declaracion de una variable int que recibira datos ingresados por el usuario 
+System.out.println("Ingrese el segundo numero");//print que le pide al usuario que ingrese un numero
+int B = in.nextInt();//declaracion de una variable int que recibira datos ingresados por el usuario
+System.out.println("Ingrese el tercer numero");//print que le pide al usuario que ingrese un numero
+int C = in.nextInt();//declaracion de una variable int que recibira datos ingresados por el usuario
+
+if((A % 10 == B % 10)||(A % 10 == C % 10)||(B % 10 == C % 10))//condicional if que se cumple si el reciduo de las divisiones de las variables es igual
+System.out.println("El resultado es: true");//print que se imprime si el if se cumple
+
+else//else que se ejecuta si no se cumple la condicion del if
+System.out.println("El resultado es: false");//print que se imprime si no se cumplio la if 
+}
+}
