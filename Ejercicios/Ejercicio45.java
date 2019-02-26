8279c54cce75b5075909624c4c54166333b8d7c4
al159799-BrandonGarcia <46730068+al159799BrandonGarcia@users.noreply.github.com>
2019-02-25 19:06:12 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio45.java
@@ -0,0 +1,13 @@
+//Nombre:Brandon Garcia
+//Fecha:25/02/2019
+//Hora:6:54pm
+
+
+import java.io.File;//importacion de la libreria file para trabajar con archivos externos 
+public class Ejercicio45{
+public static void main(String[] args){//inicializacion de main
+
+System.out.println("C:/Users/CCSTORE usuario/Desktop/Trabajo y tarea/Ejercicios progra 2/texto.txt  : " + new File("texto.txt").length() + " bytes");//print que muetra el tamaño en byts del archivo buscado 
+System.out.println("C:/Users/CCSTORE usuario/Desktop/Trabajo y tarea/Ejercicios progra 2/prueba.txt : " + new File("prueba.txt").length() + " bytes");//print que muetra el tamaño en byts del archivo buscado 
+ }
+}
