37daf92e9f7dc992f9fc928d5921bf1bb8ef63ec
al159799-BrandonGarcia <46730068+al159799BrandonGarcia@users.noreply.github.com>
2019-02-25 19:17:46 -0700

    Add files via upload

---
+++ Ejercicios/Ejercicio46.java
@@ -0,0 +1,10 @@
+//Nombre:Brandon Garcia
+//Fecha:25/02/2019
+//Hora:6:54pm
+
+public class Ejercicio46{
+public static void main(String[] args){//inicializacion de main
+
+System.out.printf("\nTiempo actual del sistema: %tc", System.currentTimeMillis());//print para mostrar la hora del sistema con la funcion currenttimemillis
+ }
+}
